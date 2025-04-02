3b1a88165afc4fa425a1a0d3c428433db4f22cef
Alok Mishra <21se02cs042@ppsu.ac.in>
2025-04-02 18:10:00 +0530

    some changes

--- index.ts
+++ index.ts
@@ -6,7 +6,12 @@ import cookieParser from "cookie-parser";
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/auth.controller";
+import {
+  loginUser,
+  registerUser,
+  logoutUser,
+} from "../controllers/auth.controller";
 
 const authRouter = Router();
 
 authRouter.post("/signup", registerUser);
 authRouter.post("/login", loginUser);
+authRouter.post("/logout", logoutUser);
 
 export default authRouter;
